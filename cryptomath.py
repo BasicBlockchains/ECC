b1f8c734275f51f999c661b8f0676ac9bed6481a
Greg Doyle <drgregdoyle@gmail.com>
2022-07-30 11:10:04 -0400

    Changing comments format

--- cryptomath.py
+++ cryptomath.py
@@ -1,7 +1,4 @@
-'''
-Mathematical methods used in elliptic curve cryptography
-'''
-
+# --- Mathematical methods used in elliptic curve cryptography --- #
 
 def legendre_symbol(n: int, p: int) -> int:
     '''
@@ -27,19 +24,20 @@ def tonelli_shanks(n: int, p: int):
     '''
     If n is a quadratic residue mod p, then we return an integer r such that r^2 = n (mod p).
     '''
-    '''Verify n is a QR'''
+
+    # Verify n is a quadratic residue
     if legendre_symbol(n, p) == -1:
         return None
 
-    '''Trivial case'''
+    # Trivial case
     if n % p == 0:
         return 0
 
-    '''p = 3 (mod 4) case'''
+    # p = 3 (mod 4) case
     if p % 4 == 3:
         return pow(n, (p + 1) // 4, p)
 
-    '''General case'''
+    ###--- General Case ---###
     # 1) Divide p-1 into its even and odd components by p-1 = 2^s * Q, where Q is odd and s >=1
     Q = p - 1
     s = 0
@@ -62,7 +60,6 @@ def tonelli_shanks(n: int, p: int):
     while t != 1:
 
         # First find the least integer i such that t^(2^i) = 1 (mod p)
-        # Note that t^(2^2) = t^2
         i = 0
         factor = t
         while factor != 1:
@@ -70,12 +67,11 @@ def tonelli_shanks(n: int, p: int):
             factor = (factor * factor) % p
 
         # Reassign variables
-
         exp = 2 ** (M - i - 1)
         b = pow(c, exp, p)
         M = i
         c = (b * b) % p
-        t = (t * b * b) % p
+        t = (t * c) % p
         R = (R * b) % p
 
     return R
