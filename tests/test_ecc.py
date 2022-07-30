acabf44e20bada14f0411678d7b6ed3ea762f077
Greg Doyle <drgregdoyle@gmail.com>
2022-07-30 10:21:54 -0400

    Changing/testing formatting to see what it looks like on GitHub

--- tests/test_ecc.py
+++ tests/test_ecc.py
@@ -1,12 +1,8 @@
-# ---------------#
-#   IMPORTS     #
-# ---------------#
+# ---IMPORTS---#
 
 from elliptic_curve import CurveFactory
 
-# ---------------#
-#   CONSTANTS   #
-# ---------------#
+# ---CONSTANTS---#
 
 A = 0
 B = 7
@@ -16,9 +12,7 @@ GENERATOR_X = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
 GENERATOR_Y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
 
 
-# ---------------#
-#   TESTS        #
-# ---------------#
+# ---TESTS---#
 
 
 def test_factory():
